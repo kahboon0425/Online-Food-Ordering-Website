4766bc6ca68da254a29f7680770d3c1b782807f6
Kah Boon <changkahboon25@gmail.com>
2023-12-12 18:51:58 +0800

    Add Cart

--- src/app/food-page/food-page.component.ts
+++ src/app/food-page/food-page.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit} from '@angular/core';
 import { Food } from '../shared/models/Food';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FoodService } from '../services/food/food.service';
+import { CartService } from '../services/cart/cart.service';
 
 @Component({
   selector: 'app-food-page',
@@ -12,7 +13,10 @@ import { FoodService } from '../services/food/food.service';
 export class FoodPageComponent implements OnInit{
 
   food!: Food;
-  constructor(private activatedRoute:ActivatedRoute, private foodService: FoodService){
+  constructor(private activatedRoute:ActivatedRoute, 
+    private foodService: FoodService,
+    private cartService: CartService,
+    private router: Router){
     activatedRoute.params.subscribe((params) => {
       if(params['id'])
         this.food = foodService.getFoodById(params['id']);
@@ -20,5 +24,10 @@ export class FoodPageComponent implements OnInit{
   }
 
   ngOnInit(): void{}
+
+  addToCart(){
+    this.cartService.addToCart(this.food);
+    this.router.navigateByUrl('/cart-page');
+  }
 
 }
